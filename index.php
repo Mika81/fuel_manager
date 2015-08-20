706009311a6519e3530768372f3e2861ab1f5d9f
mika81 <webdeveloper@diw.fr>
2015-08-20 11:17:30 +0200

    Modifies index.php

--- index.php
+++ index.php
@@ -3,5 +3,22 @@
 
 session_start();
 $msg = "";
+require_once 'functions/functions.php';
+require_once 'controller/controller.php';
+
+$manager = new UserManager($db);
+
+/* Create user */
+if(isset($_POST['create'])){
+    $user = array(
+        'alias' => $_POST['alias'],
+        'pwd' => $_POST['pwd'],
+        'address' => $_POST['address'],
+        'email' => $_POST['email'],
+    );
+    $newUser = new User($user);
+    $manager->add($newUser);
+}
+
 
 include_once 'views/index.tpl.php';
