96f47ff76b7ef08e193fa49d5b80c99906e80d01
mika81 <webdeveloper@diw.fr>
2015-08-20 11:15:28 +0200

    Adds debug elements

--- functions/functions.php
+++ functions/functions.php
@@ -1,8 +1,15 @@
 <?php
 ## functions/functions.php
 
+/* Debug function */
 function d($arg) {
     print "<pre>";
     print_r($arg);
     print "</pre>";
 }
+/* DEBUG */
+print "<br>$ SESSION :";
+d($_SESSION);
+print "<br>$ POST :";
+d($_POST);
+/* -------------------- */
