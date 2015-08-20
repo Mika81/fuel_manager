788b033f56f096c300f6ef3ef3b623898f392c55
mika81 <webdeveloper@diw.fr>
2015-08-20 15:59:29 +0200

    Creates UserController.class file - not a class for the moment -> TO CHANGE

---
+++ controller/UserController.class.php
@@ -0,0 +1,53 @@
+<?php
+
+/* Create user */
+if(isset($_POST['create'])) :
+    $user = array(
+        'alias' => $_POST['alias'],
+        'pwd' => $_POST['pwd'],
+        'address' => $_POST['address'],
+        'email' => $_POST['email'],
+    );
+    $newUser = new User($user);
+    $user_exists = $user_manager->exists($newUser);
+    if($user_exists) :
+        $msg .= "<p>Cet e-mail ou ce nom d'utilisateurs existe déjà</p>";
+    else :
+        $user_manager->add($newUser);
+        $msg .= "<p>Vous pouvez vous connecter</p>";
+    endif;
+endif;
+
+if(isset($_POST['connection']) && !empty($_POST['alias']) && !empty($_POST['pwd'])) :
+    $user = array(
+        'alias' => $_POST['alias'],
+        'pwd' => $_POST['pwd'],
+    );
+    $current_user_exists = $user_manager->user_start_session($user);
+    if($current_user_exists):
+        $current_user = $user_manager->get($user);
+        $_SESSION['user']['alias'] = $current_user->alias;
+        $_SESSION['user']['email'] = $current_user->email;
+        $_SESSION['user']['address'] = $current_user->address;
+    else :
+        $msg .= "Nom ou mot de passe incorrect";
+    endif;
+else :
+    if(isset($_POST['connection'])) :
+        $msg .= "Tous les champs doivent être complété";
+    endif;
+endif;
+
+if(isset($_POST['logout'])){
+    unset($_SESSION['user']);
+    session_destroy();
+}
+
+/**
+ * Description of UserController
+ *
+ * @author mika
+ */
+//class UserController {
+//    //put your code here
+//}
