ca5743cdbc215d6045387d1bd402188d494dc084
mika81 <webdeveloper@diw.fr>
2015-08-20 16:02:39 +0200

    Creates user_start_session function

--- model/user/UserManager.class.php
+++ model/user/UserManager.class.php
@@ -18,9 +18,10 @@ class UserManager {
     public function setDb(PDO $db) {
         $this->db = $db;
     }
-    
+
     /* ----------CREATE */
-    public function add(User $user){
+
+    public function add(User $user) {
         $query = $this->db->prepare('INSERT INTO user SET alias=:alias, pwd=:pwd, address=:address, email=:email');
         $query->bindValue(':alias', $user->getAlias());
         $query->bindValue(':pwd', $user->getPwd());
@@ -28,29 +29,60 @@ class UserManager {
         $query->bindValue(':email', $user->getEmail());
         $query->execute();
     }
-    
+
     /* ----------READ */
-    public function get(Array $user){
+
+    public function get(Array $user) {
         $query = $this->db->prepare('SELECT * FROM user WHERE alias=:alias AND pwd=:pwd');
         $query->bindValue(':alias', $user['alias']);
-        $query->bindValue(':pwd', $user['pwd']);
+        $query->bindValue(':pwd', hash('sha512', $user['pwd']));
         $query->execute();
         $data = $query->fetch(PDO::FETCH_ASSOC);
         return new User($data);
     }
-    
-    public function getList(){
+
+    public function getList() {
         /* Get user list */
     }
-    
+
     /* ----------UPDATE */
-    public function update(User $user){
+
+    public function update(User $user) {
         /* Update current user */
     }
-    
+
     /* ----------DELETE */
-    public function delete(User $user){
+
+    public function delete(User $user) {
         /* Delete current user */
+    }
+
+    /* ----------ALIAS/EMAIL EXISTS? */
+
+    public function exists(User $user) {
+        $query = $this->db->prepare('SELECT * FROM user WHERE alias=:alias OR email=:email');
+        $query->bindValue(':alias', $user->getAlias());
+        $query->bindValue(':email', $user->getEmail());
+        $query->execute();
+        $data = $query->fetch(PDO::FETCH_ASSOC);
+        if (!empty($data)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function user_start_session(Array $user) {
+        $query = $this->db->prepare('SELECT * FROM user WHERE alias=:alias AND pwd=:pwd');
+        $query->bindValue(':alias', $user['alias']);
+        $query->bindValue(':pwd', hash('sha512', $user['pwd']));
+        $query->execute();
+        $data = $query->fetch(PDO::FETCH_ASSOC);
+        if (!empty($data)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
