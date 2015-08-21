06f7a93d3156b60fa411613365aa720a8ab5ca50
mika81 <webdeveloper@diw.fr>
2015-08-21 17:55:46 +0200

    Modifies navbar css

--- views/index.tpl.php
+++ views/index.tpl.php
@@ -2,25 +2,27 @@
 include_once 'head.html';
 include_once 'header.html';
 include_once 'nav.tpl.php';
-?>
-    <h1> <?php print $msg; ?> </h1>
-    <?php
-    if (empty($_SESSION['user'])) :
-        if (isset($_POST['signup'])) :
-            include_once 'forms/sign_up_form.html';
-        else :
-            include_once 'forms/log_in_form.html';
-        endif;
-    else:
-        $msg .= "La session n'est pas vide";
-    endif;
-    if($_SESSION['profil']):
-        include_once 'profil.tpl.php';
-    endif;
-    if($_SESSION['manager']):
-        include_once 'manager.tpl.php';
-    endif;
+if (!empty($msg)):
     ?>
+    <div class="alert alert-info"><p><?php print $msg; ?> </p></div>
+    <?php
+endif;
+if (empty($_SESSION['user'])) :
+    if (isset($_POST['signup'])) :
+        include_once 'forms/sign_up_form.html';
+    else :
+        include_once 'forms/log_in_form.html';
+    endif;
+else:
+    $msg .= "La session n'est pas vide";
+endif;
+if ($_SESSION['profil']):
+    include_once 'profil.tpl.php';
+endif;
+if ($_SESSION['manager']):
+    include_once 'manager.tpl.php';
+endif;
+?>
 </body>
 <?php
 include_once 'footer.html';
--- views/nav.tpl.php
+++ views/nav.tpl.php
@@ -1,12 +1,24 @@
-<nav>
-    <a href='<?php print "http://" . BASE_URL ?>'>Home</a>
-    <?php if (!empty($_SESSION)): ?>
-        <div class="alert alert-info">
-            <p>Bonjour <?php print $_SESSION['user']['alias']; ?> !
-                <?php
-                include_once 'forms/log_out_form.html';
-                ?>
-            </p>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="<?php print "http://" . BASE_URL ?>">--</a>
         </div>
-    <?php endif; ?>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <?php if (!empty($_SESSION)): ?>
+                    <li class="navbar-text">Bonjour <?php print $_SESSION['user']['alias']; ?> !</li>
+                    <li>
+                        <?php
+                        include_once 'forms/log_out_form.html';
+                        ?>
+                    </li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </div>
 </nav>
