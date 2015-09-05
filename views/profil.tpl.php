493e85cd4b81ae741d0a27b936f7ae91c1518c98
Mika <webdeveloper@diw.fr>
2015-09-05 15:40:58 +0200

    Modifies details for UX in profil page

--- views/profil.tpl.php
+++ views/profil.tpl.php
@@ -21,47 +21,6 @@
         </tbody>
     </table>
 </div>
-    
-<?php
-if(isset($_SESSION['vehicle']) && !empty($_SESSION['vehicle'])) :
-?>
-<h3>Véhicule sélectionné</h3>
-    <div class="table-responsive">
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>Marque</th>
-                    <th>Modèle</th>
-                    <th>Type</th>
-                    <th>Carburant</th>
-                    <th>Kilométrage</th>
-                    <th>Voir les statistiques de ce véhicule</th>
-                    <th>Gérer le véhicule</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    print "<tr>";
-                    print "<td>" . ucfirst($_SESSION['vehicle']['brand']) . "</td>";
-                    print "<td>" . ucfirst($_SESSION['vehicle']['model']) . "</td>";
-                    print "<td>" . ucfirst($_SESSION['vehicle']['type']) . "</td>";
-                    print "<td>" . ucfirst($_SESSION['vehicle']['fuel_type']) . "</td>";
-                    print "<td>" . ucfirst($_SESSION['vehicle']['global_km']) . " km</td>";
-                    print "<td><form method='post'><button type='submit' name='stats' value='".$_SESSION['vehicle']['vehicle_id']."'>O</button></form></td>";
-                    print "<td><form method='post'><button type='submit' name='manage_vehicle' value='".$_SESSION['vehicle']['vehicle_id']."'>O</button></form></td>";
-                    print "<tr>";
-                ?>
-            </tbody>
-        </table>
-    </div>
-<?php
-else :
-?>
-    <h4>Sélectionnez un véhicule dans la liste de vos véhicules</h4>
-<?php
-endif;
-?>
-
 
 <h3>Mes véhicules</h3>
 <?php
@@ -76,7 +35,8 @@ if (is_array($vehicle_list)) :
                     <th>Type</th>
                     <th>Carburant</th>
                     <th>Kilométrage</th>
-                    <th>Sélectionner</th>
+                    <th>Voir les statistiques de ce véhicule</th>
+                    <th>Gérer le véhicule</th>
                     <th>Supprimer</th>
                 </tr>
             </thead>
@@ -89,7 +49,8 @@ if (is_array($vehicle_list)) :
                     print "<td>" . ucfirst($value['type']) . "</td>";
                     print "<td>" . ucfirst($value['fuel_type']) . "</td>";
                     print "<td>" . ucfirst($value['global_km']) . " km</td>";
-                    print "<td><form method='post'><button type='submit' name='vehicle_selected' value='".$value['vehicle_id']."'>O</button></form></td>";
+                    print "<td><form method='post'><button type='submit' name='stats' value='".$_SESSION['vehicle']['vehicle_id']."'>O</button></form></td>";
+                    print "<td><form method='post'><button type='submit' name='manage_vehicle' value='".$_SESSION['vehicle']['vehicle_id']."'>O</button></form></td>";
                     print "<td><form method='post'><button type='submit' name='vehicle_2_delete' value='".$value['vehicle_id']."'>X</button></form></td>";
                     print "<tr>";
                 endforeach;
