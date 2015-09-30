31f99d9d1c28dfc0aa6e685e8dea2ea54fd9ed88
Mika <webdeveloper@diw.fr>
2015-09-30 16:42:15 +0200

    Adds router and TripController files, creates TripManeger object

--- index.php
+++ index.php
@@ -4,14 +4,17 @@
 $msg = "";
 require_once 'src/functions.php';
 require_once 'controller/controller.php';
+require_once 'controller/router.php';
 
 session_start();
 
 $user_manager = new UserManager($db);
 $vehicle_manager = new VehicleManager($db);
+$trip_manager = new TripManager($db);
 
 require_once 'controller/UserController.class.php';
 require_once 'controller/VehicleController.class.php';
+require_once 'controller/TripController.class.php';
 
 include_once 'views/index.tpl.php';
 
