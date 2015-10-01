65fdfe578118f4e23a310ca1cfb4aa6a24954420
Mika <webdeveloper@diw.fr>
2015-10-01 16:40:30 +0200

    Modifies templates and add image on homepage

--- views/index.tpl.php
+++ views/index.tpl.php
@@ -2,20 +2,39 @@
 include_once 'head.html';
 include_once 'header.html';
 include_once 'nav.tpl.php';
+
 if (!empty($msg)):
     ?>
     <div class="alert alert-info"><p><?php print $msg; ?> </p></div>
     <?php
 endif;
-if (empty($_SESSION['user'])) :
-    if (isset($_POST['signup'])) :
-        include_once 'forms/sign_up_form.html';
-    else :
-        include_once 'forms/log_in_form.html';
-    endif;
-else:
-    $msg .= "La session n'est pas vide";
+
+if (empty($_SESSION)):
+    ?>
+    <div class='container-fluid'>
+        <div class='row'>
+            <div class='col-md-6 col-md-offset-3 text-center m-bottom-50'>
+                <img src='../src/img/car.png' alt='blue car'>
+                <h1>FUEL.DIW.FR</h1>
+                <h2>L'application pour gérer votre carburant</h2>
+                <p>Connectez-vous ou créez un compte</p>
+            </div>
+            <div class='col-md-6 col-md-offset-3'>
+                <?php
+                if (empty($_SESSION['user'])) :
+                    if (isset($_POST['signup'])) :
+                        include_once 'forms/sign_up_form.html';
+                    else :
+                        include_once 'forms/log_in_form.html';
+                    endif;
+                endif;
+                ?>
+            </div>
+        </div>
+    </div>
+    <?php
 endif;
+
 if ($_GET['profil']):
     include_once 'profil.tpl.php';
 endif;
@@ -27,7 +46,6 @@ if ($_GET['stats']):
 endif;
 ?>
 </div> <!-- End .container-fluid .container-global -->
-</body>
 <?php
 include_once 'footer.html';
 ?>
--- views/stats.tpl.php
+++ views/stats.tpl.php
@@ -4,9 +4,16 @@
 
 <h3>Gérer votre <?php echo $vehicle_object->getBrand() ." ". $vehicle_object->getModel(); ?></h3>
 <h4>Kilométrage total: <?php echo $vehicle_object->getGlobal_km(); ?> km</h4>
-<p>Consommation moyenne: ?? /100km</p>
-<p>Coût moyen: ?? €/km</p>
-<p>Date du dernier plein: <?php print $last_modif; ?></p>
-<p>Carburant total utilisé depuis le jj/mm/aaaa : ?? litres</p>
-<p>Coût total utilisé depuis le jj/mm/aaaa : ?? €</p>
+<div class="alert alert-success">
+    <p>Consommation moyenne: <strong><?php echo $average_conso; ?></strong> litres/100km</p>
+    <p>Coût moyen: <strong><?php echo $price_per_km; ?></strong> €/km</p>
+</div>
+<div class="alert alert-info">
+    <h4>Depuis le <?php echo $first_date; ?>:</h4>
+    <p><strong><?php echo $global_distance; ?></strong> km parcourus,<br>
+        <strong><?php echo $global_conso; ?></strong> litres de carburant consommés,<br>
+        pour un coût global de <strong><?php echo $global_price; ?></strong> €uros.</p>
+    <p>Date d'enregistrement du dernier plein: <strong><?php echo $last_modif; ?></strong></p>
+</div>
+
 
