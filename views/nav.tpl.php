827fc8b2c1ca7daa076dd9ce87d5749601d51fd5
Mika <webdeveloper@diw.fr>
2015-10-02 10:16:11 +0200

    Adding/suppressing bootstrap theme files, modifies nav

--- views/nav.tpl.php
+++ views/nav.tpl.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-inverse">
     <div class="container-fluid">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -12,7 +12,7 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
                 <?php if (!empty($_SESSION)): ?>
-                    <li class="navbar-text">Bonjour <?php print $_SESSION['user']['alias']; ?> !</li>
+                    <li class="navbar-text m-left-10">Bonjour <?php print $_SESSION['user']['alias']; ?> !</li>
                     <li>
                         <?php
                         include_once 'forms/log_out_form.html';
